a8237175303f9cfc3815f064722051e6ad578622
TZ <atian25@qq.com>
2017-11-01 15:22:34 +0800

    fix: fastReady default to false

--- config/config.default.js
+++ config/config.default.js
@@ -4,13 +4,13 @@
  * @member Config#development
  * @property {Array} watchDirs - dirs needed watch, when files under these change, application will reload, use relative path
  * @property {Array} ignoreDirs - dirs don't need watch, including subdirectories, use relative path
- * @property {Boolean} fastReady - don't wait all plugins ready, default is true.
+ * @property {Boolean} fastReady - don't wait all plugins ready, default is false.
  * @property {Boolean} reloadOnDebug - whether reload on debug, default is true.
  * @see https://github.com/eggjs/egg-development-proxyworker to keep debug port when restart
  */
 exports.development = {
   watchDirs: [],
   ignoreDirs: [],
-  fastReady: true,
+  fastReady: false,
   reloadOnDebug: true,
 };
