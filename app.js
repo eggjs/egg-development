433d654208b3e4e860c456215f01d5f742cd22cd
TZ | 天猪 <atian25@qq.com>
2017-11-01 03:32:56 -0500

    fix: fastReady default to false (#11)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = app => {
-  // don't need to wait at local development mode
+  // if true, then don't need to wait at local development mode
   if (app.config.development.fastReady) process.nextTick(() => app.ready(true));
 };
--- test/fixtures/development/config/config.default.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-exports.development = {
-  fastReady: false,
-};
