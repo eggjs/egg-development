473ab2a3c9d56f91cd085fbcdd698e7dec5f6a35
TZ <atian25@qq.com>
2017-12-08 12:14:23 +0800

    docs: add overrideDefault to README

--- config/config.default.js
+++ config/config.default.js
@@ -6,7 +6,7 @@
  * @property {Array} ignoreDirs - dirs don't need watch, including subdirectories, use relative path
  * @property {Boolean} fastReady - don't wait all plugins ready, default is false.
  * @property {Boolean} reloadOnDebug - whether reload on debug, default is true.
- * @property {Boolean} overrideDefault - whether overrider default watchDirs, default is false.
+ * @property {Boolean} overrideDefault - whether override default watchDirs, default is false.
  * @see https://github.com/eggjs/egg-development-proxyworker to keep debug port when restart
  */
 exports.development = {
