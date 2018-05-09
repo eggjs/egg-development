fee304dc26ca787a465469ca8e033e5815771fdc
popomore <sakura9515@gmail.com>
2018-05-10 00:45:22 +0800

    f

--- test/timing.test.js
+++ test/timing.test.js
@@ -4,9 +4,10 @@ const mm = require('egg-mock');
 const fs = require('mz/fs');
 const path = require('path');
 const mkdirp = require('mz-modules/mkdirp');
+const assert = require('assert');
 
 
-describe.only('test/timing.test.js', () => {
+describe('test/timing.test.js', () => {
   const timingJSON = path.join(__dirname, 'fixtures/timing/run/agent_timing_11111.json');
   let app;
   before(function* () {
@@ -26,11 +27,24 @@ describe.only('test/timing.test.js', () => {
       .expect('false');
   });
 
-  it('should clean all timing json in agent', function* () {
+  it('should render page', function* () {
     const res = yield app.httpRequest()
       .get('/__loader_trace__');
 
-    const json = res.text.match(/data = (.*?);/);
-    console.log(JSON.parse(json));
+    let json = res.text.match(/data = (.*?);/);
+    json = JSON.parse(json[1]);
+    assert(json.length === 100);
+
+    const first = json[0];
+    assert(first.type === 'agent');
+    assert(typeof first.pid === 'string');
+    assert.deepEqual(first.range, [ first.start, first.end ]);
+    assert(first.title === 'agent(0)');
+
+    const last = json[json.length - 1];
+    assert(/^app_\d+$/.test(last.type));
+    assert(typeof last.pid === 'string');
+    assert.deepEqual(last.range, [ last.start, last.end ]);
+    assert(/^app_\d+\(65\)$/.test(last.title));
   });
 });
