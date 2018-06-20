c5d94fb200f4229f4018ba07f5fbb215db1a9713
popomore <sakura9515@gmail.com>
2018-06-20 20:35:31 +0800

    f

--- test/timing.test.js
+++ test/timing.test.js
@@ -53,6 +53,6 @@ describe('test/timing.test.js', () => {
     assert(/^app_\d+$/.test(last.type));
     assert(typeof last.pid === 'string');
     assert.deepEqual(last.range, [ last.start, last.end ]);
-    assert(/^app_\d+\(65\)$/.test(last.title));
+    assert(/^app_\d+\(66\)$/.test(last.title));
   });
 });
